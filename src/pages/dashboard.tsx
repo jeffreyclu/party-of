2efc8ed26fb66577bc4a4465c7eaf9f3872dd792
Jeffrey Lu <hi@jeffreyclu.com>
2024-09-11 14:49:36 -0400

    render map component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { useUser } from "../hooks/use-user";
-import { useLogout } from '../hooks/use-logout';
+import Map from '../components/map';
 
 const Dashboard: React.FC = () => {
     const { user } = useUser();
-
-    const handleLogout = useLogout();
 
     if (!user) {
         return <div>Loading...</div>;
@@ -14,9 +12,7 @@ const Dashboard: React.FC = () => {
     return (
         <div>
             <h2>Welcome, {user.displayName}</h2>
-            {user.photoURL && user.displayName && <img src={user.photoURL} alt={user.displayName} />}
-            <p>Email: {user.email}</p>
-            <button onClick={handleLogout}>Logout</button>
+            <Map />
         </div>
     );
 };
