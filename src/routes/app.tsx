b3a6e5c753ad2e631e783336066241aec4d8d3ed
Jeffrey Lu <hi@jeffreyclu.com>
2024-09-12 12:08:14 -0400

    remove settings

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -3,9 +3,10 @@ import ProtectedRoute from "../components/protected-route";
 import { useUser } from "../hooks/use-user";
 import Dashboard from "../pages/dashboard";
 import Login from "../pages/login";
-import Settings from "../pages/settings";
 import Map from "../components/map";
 import FavoriteRestaurants from "../components/favorites";
+import NotFound from "../pages/404";
+import Profile from "../pages/profile";
 
 export const AppRoutes: React.FC = () => {
     const { user } = useUser();
@@ -14,10 +15,11 @@ export const AppRoutes: React.FC = () => {
         <Routes>
             <Route path="/login" element={user ? <Navigate to="/dashboard" /> : <Login />} />
             <Route path="/dashboard" element={<ProtectedRoute component={Dashboard} />} />
-            <Route path="/settings" element={<ProtectedRoute component={Settings} />} />
             <Route path="/map" element={<ProtectedRoute component={Map} />} />
             <Route path="/favorites" element={<ProtectedRoute component={FavoriteRestaurants} />} />
+            <Route path="/profile" element={<ProtectedRoute component={Profile} />} />
             <Route path="/" element={user ? <Navigate to="/dashboard" /> : <Navigate to="/login" />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     );
 };
