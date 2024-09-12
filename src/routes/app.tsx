6bd066c5b9261d14e84923d61fbe1c70d395bda7
Jeffrey Lu <hi@jeffreyclu.com>
2024-09-12 11:08:58 -0400

    add new pages

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -3,18 +3,20 @@ import ProtectedRoute from "../components/protected-route";
 import { useUser } from "../hooks/use-user";
 import Dashboard from "../pages/dashboard";
 import Login from "../pages/login";
+import Settings from "../pages/settings";
+import Map from "../components/map";
+import FavoriteRestaurants from "../components/favorites";
 
 export const AppRoutes: React.FC = () => {
-    const { user, loading } = useUser();
-
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+    const { user } = useUser();
 
     return (
         <Routes>
             <Route path="/login" element={user ? <Navigate to="/dashboard" /> : <Login />} />
             <Route path="/dashboard" element={<ProtectedRoute component={Dashboard} />} />
+            <Route path="/settings" element={<ProtectedRoute component={Settings} />} />
+            <Route path="/map" element={<ProtectedRoute component={Map} />} />
+            <Route path="/favorites" element={<ProtectedRoute component={FavoriteRestaurants} />} />
             <Route path="/" element={user ? <Navigate to="/dashboard" /> : <Navigate to="/login" />} />
         </Routes>
     );
