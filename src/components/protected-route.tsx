f21160ff59c5d91f9f1033775a06110f477cdd86
Jeffrey Lu <hi@jeffreyclu.com>
2024-09-12 11:07:47 -0400

    add loading to protected route

--- src/components/protected-route.tsx
+++ src/components/protected-route.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useUser } from '../hooks/use-user';
+import Loading from './loading';
 
 interface ProtectedRouteProps {
     component: React.ComponentType;
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ component: Component }) => {
-    const { user } = useUser();
+    const { user, loadingUser } = useUser();
+
+    if (loadingUser) {
+        return <Loading />; // You can replace this with a proper loading indicator
+    }
 
     return user ? <Component /> : <Navigate to="/login" />;
 };
