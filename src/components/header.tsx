09e9c6b14e715a6b51a43683e6a7284cab2ee081
Jeffrey Lu <hi@jeffreyclu.com>
2024-09-12 11:01:56 -0400

    update header with logo svg

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useUser } from '../hooks/use-user';
 
-import logo from '../assets/temp-logo.png';
+import logo from '../assets/logo.svg';
 import './header.css';
 import { useLogout } from '../hooks/use-logout';
 import { Link } from 'react-router-dom';
+import LoginButton from './login-button';
 
 const Header: React.FC = () => {
-    const { user } = useUser();
+    const { user, loadingUser } = useUser();
 
     const handleLogout = useLogout();
 
@@ -20,21 +21,23 @@ const Header: React.FC = () => {
             <nav>
                 <div className="logo-container">
                     <Link to="/">
-                        <img src={logo} alt="Logo" className="logo" />
+                        <img src={logo} alt="Party Of Logo" className="logo" />
                     </Link>
                 </div>
-                {user && (
-                    <div className="user-info">
-                        <span>{user.displayName}</span>
-                        {user.photoURL ? (
-                            <img src={user.photoURL} alt={user.displayName || 'User'} className="user-photo" />
-                        ) : (
-                            <div className="user-initial">
-                                {user.displayName ? getInitial(user.displayName) : 'U'}
-                            </div>
-                        )}
-                        <button onClick={handleLogout}>Logout</button>
-                    </div>
+                {!loadingUser && (
+                    user ? (
+                        <div className="user-info">
+                            <span>{user.displayName}</span>
+                            {user.photoURL ? (
+                                <img src={user.photoURL} alt={user.displayName || 'User'} className="user-photo" />
+                            ) : (
+                                <div className="user-initial">
+                                    {user.displayName ? getInitial(user.displayName) : 'U'}
+                                </div>
+                            )}
+                            <button onClick={handleLogout}>Logout</button>
+                        </div>
+                    ) : <LoginButton />
                 )}
             </nav>
         </header>
