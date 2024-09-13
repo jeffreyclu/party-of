85500ca1325621532cfe3950c780bdb73ed2253a
Jeffrey Lu <hi@jeffreyclu.com>
2024-09-13 16:43:28 -0400

    add create user profile function

--- src/firebase/user-functions.ts
+++ src/firebase/user-functions.ts
@@ -1,6 +1,28 @@
-import { doc, setDoc, getDocFromServer } from 'firebase/firestore';
-import { db } from './index'; // Adjust the import path as needed
+import { doc, setDoc, getDocFromServer, serverTimestamp } from 'firebase/firestore';
+import { User } from 'firebase/auth';
+
+import { db } from './index';
 import { UserProfile } from '../types';
+
+export const createUserProfile = async (user: User) => {
+    const userDocRef = doc(db, 'users', user.uid);
+    const newUserProfile: UserProfile = {
+        id: user.uid,
+        displayName: user.displayName ?? '',
+        email: user.email ?? '',
+        dietaryRestrictions: [],
+        favorites: [],
+        completedIntro: false,
+        createdAt: serverTimestamp(),
+    };
+    try {
+        await setDoc(userDocRef, newUserProfile);
+        console.log('User profile created successfully');
+    } catch (error) {
+        console.error('Error creating user profile:', error);
+        throw error;
+    }
+};
 
 export const updateUserProfile = async (uid: string, displayName: string, dietaryRestrictions: string[]) => {
     const userDocRef = doc(db, 'users', uid);
