87c759c80d3f35b7278d737bb5930521872befa3
Jeffrey Lu <hi@jeffreyclu.com>
2024-09-13 15:24:55 -0400

    add global consts

---
+++ src/consts.ts
@@ -0,0 +1,9 @@
+export const dietaryOptions = [
+    'Peanut Allergy',
+    'Dairy Intolerance',
+    'Gluten Intolerance',
+    'Celiac Disease',
+    'Shellfish Allergy',
+    'Soy Allergy',
+    'Egg Allergy',
+];
