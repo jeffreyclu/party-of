4d8ebce179e6ecda8d8ae3799689ebb31c4c5734
Jeffrey Lu <hi@jeffreyclu.com>
2024-09-13 16:30:42 -0400

    update save restaurant logic

--- src/firebase/restaurant-functions.ts
+++ src/firebase/restaurant-functions.ts
@@ -1,7 +1,7 @@
 import { doc, collection, getDocFromServer, getDocsFromServer, serverTimestamp, deleteDoc, setDoc, query, where } from 'firebase/firestore';
 import { User } from 'firebase/auth';
 import { db } from './index';
-import { Restaurant } from '../types';
+import { Favorite, Restaurant } from '../types';
 
 // Save a favorite restaurant to Firestore for a specific user
 export const saveFavoriteRestaurant = async (user: User, restaurant: Omit<Restaurant, 'addedAt'>) => {
@@ -15,23 +15,36 @@ export const saveFavoriteRestaurant = async (user: User, restaurant: Omit<Restau
     if (querySnapshot.empty) {
       // Add the restaurant details to the 'restaurants' collection if it doesn't exist
       restaurantDocRef = doc(restaurantsCollectionRef);
-      await setDoc(restaurantDocRef, restaurant);
+      await setDoc(restaurantDocRef, { ...restaurant, addedAt: serverTimestamp() });
       console.log('Restaurant details added to restaurants collection');
     } else {
       // Use the existing restaurant document reference
       restaurantDocRef = querySnapshot.docs[0].ref;
-      console.log('Restaurant already exists in restaurants collection');
-    }
+      const restaurantDoc = await getDocFromServer(restaurantDocRef);
+      const restaurantData = restaurantDoc.data();
+      const addedAt = restaurantData?.addedAt.toDate();
+      const oneMonthAgo = new Date();
+      oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
 
+      if (addedAt < oneMonthAgo) {
+        // Overwrite the existing restaurant document if addedAt is more than 1 month old
+        await setDoc(restaurantDocRef, { ...restaurant, addedAt: serverTimestamp() });
+        console.log('Restaurant details updated in restaurants collection');
+      } else {
+        console.log('Restaurant already exists and is less than 1 month old');
+      }
+    }
     // Add a reference to the restaurant document ID and the addedAt timestamp in the user's favorites
     const userDocRef = doc(db, 'users', user.uid);
     const favoritesCollection = collection(userDocRef, 'favorites');
     const newFavoriteDocRef = doc(favoritesCollection);
 
-    await setDoc(newFavoriteDocRef, {
+    const newFavorite: Favorite = {
       restaurantId: restaurantDocRef.id,
       addedAt: serverTimestamp(), // Use Firestore server timestamp
-    });
+    }
+
+    await setDoc(newFavoriteDocRef, newFavorite);
     console.log('Restaurant reference added to user favorites');
   } catch (error) {
     console.error('Error adding restaurant to favorites:', error);
