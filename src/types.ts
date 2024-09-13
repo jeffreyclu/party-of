8fb76cfad3a19909f97321019b768c52e066c7b0
Jeffrey Lu <hi@jeffreyclu.com>
2024-09-13 15:24:46 -0400

    add more global types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import { Timestamp } from "firebase/firestore";
+
 export interface Restaurant {
     id: string;
     name: string;
@@ -5,4 +7,29 @@ export interface Restaurant {
     lng: number;
     address: string;
     addedAt: Date;
+}
+
+export interface Invite {
+    id: string;
+    senderId: string;
+    recipientId: string;
+    eventDate: Timestamp;
+    eventType: EventType;
+    status: InviteStatus;
+    senderFavorites: Restaurant["id"][];
+    recipientFavorites: Restaurant["id"][];
+    suggestedRestaurants: Restaurant["id"][];
+}
+
+export enum InviteStatus {
+    Pending = 'Pending',
+    Accepted = 'Accepted',
+    Declined = 'Declined',
+}
+
+export enum EventType {
+    Lunch = 'Lunch',
+    Dinner = 'Dinner',
+    Drinks = 'Drinks',
+    Other = 'Other',
 }
