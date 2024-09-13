19d5378f5341663077e143d64ac74c7bfa3e94dc
Jeffrey Lu <hi@jeffreyclu.com>
2024-09-13 15:28:13 -0400

    add user state to restaurants context

--- src/context/restaurants.tsx
+++ src/context/restaurants.tsx
@@ -8,24 +8,35 @@ interface FavoriteRestaurantsContextType {
     setFavoriteRestaurants: React.Dispatch<React.SetStateAction<Restaurant[]>>;
     addFavoriteRestaurant: (restaurant: Restaurant) => Promise<void>;
     removeFavoriteRestaurant: (restaurantId: string) => Promise<void>;
+    loadingFavoriteRestaurants: boolean;
 }
 
 export const FavoriteRestaurantsContext = createContext<FavoriteRestaurantsContextType | undefined>(undefined);
 
 export const FavoriteRestaurantsProvider = ({ children }: { children: ReactNode }) => {
     const [favoriteRestaurants, setFavoriteRestaurants] = useState<Restaurant[]>([]);
-    const { user } = useUser();
+    const [loadingFavoriteRestaurants, setLoadingFavoriteRestaurants] = useState<boolean>(true);
+    const { user, loadingUser } = useUser();
 
     useEffect(() => {
         const fetchFavoriteRestaurants = async () => {
-        if (user) {
-            const restaurants = await getFavoriteRestaurants(user);
-            setFavoriteRestaurants(restaurants || []);
-        }
+            try {
+                if (user) {
+                    const restaurants = await getFavoriteRestaurants(user);
+                    console.log('Retrieved favorite restaurants:', restaurants);
+                    setFavoriteRestaurants(restaurants || []);
+                }
+            } catch (error) {
+                console.error('Error fetching favorite restaurants: ', error);
+            } finally {
+                setLoadingFavoriteRestaurants(false);
+            }
         };
 
-        fetchFavoriteRestaurants();
-    }, [user]);
+        if (!loadingUser) {
+            fetchFavoriteRestaurants();
+        }
+    }, [user, loadingUser]);
 
     const addFavoriteRestaurant = async (restaurant: Restaurant) => {
         if (user) {
@@ -42,8 +53,8 @@ export const FavoriteRestaurantsProvider = ({ children }: { children: ReactNode 
     };
 
     return (
-        <FavoriteRestaurantsContext.Provider value={{ favoriteRestaurants, setFavoriteRestaurants, addFavoriteRestaurant, removeFavoriteRestaurant }}>
-        {children}
+        <FavoriteRestaurantsContext.Provider value={{ favoriteRestaurants, setFavoriteRestaurants, addFavoriteRestaurant, removeFavoriteRestaurant, loadingFavoriteRestaurants }}>
+            {children}
         </FavoriteRestaurantsContext.Provider>
     );
 };
