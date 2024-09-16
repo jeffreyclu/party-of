5f944eb40aab7cf1a359cc9d82a39863d0c50a4f
Jeffrey Lu <hi@jeffreyclu.com>
2024-09-15 21:31:44 -0400

    remove unused consts

--- src/consts.ts
+++
@@ -1,9 +0,0 @@
-export const dietaryOptions = [
-    'Peanut Allergy',
-    'Dairy Intolerance',
-    'Gluten Intolerance',
-    'Celiac Disease',
-    'Shellfish Allergy',
-    'Soy Allergy',
-    'Egg Allergy',
-];
