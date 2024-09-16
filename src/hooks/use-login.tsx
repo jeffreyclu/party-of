1fd3073f3368fb76ff6f047200b50e090aac5840
Jeffrey Lu <hi@jeffreyclu.com>
2024-09-16 10:36:33 -0400

    use userprofile and user contexts in login flow

--- src/hooks/use-login.tsx
+++ src/hooks/use-login.tsx
@@ -3,14 +3,16 @@ import { useNavigate } from 'react-router-dom';
 import { auth, provider } from '../firebase/index';
 import { signInWithPopup, setPersistence, browserLocalPersistence } from 'firebase/auth';
 import { useUser } from './use-user';
-import { createUserProfile, getUserProfile } from '../firebase/user-functions';
+// import { getUserProfile } from '../firebase/user-functions';
 import { useToast } from './use-toast';
 import { ToastType } from '../types';
+import { useUserProfile } from './use-user-profile';
 
 export const useLogin = () => {
     const navigate = useNavigate();
     const { setUser } = useUser();
     const { showToast } = useToast();
+    const { createUserProfile, userProfileData } = useUserProfile();
 
     const handleLogin = async () => {
         try {
@@ -18,15 +20,15 @@ export const useLogin = () => {
             const result = await signInWithPopup(auth, provider);
             const user = result.user;
 
-            const userProfile = await getUserProfile(user.uid)
+            setUser(user);
+
+            const userProfile = userProfileData;
 
             if (!userProfile) {
                 createUserProfile(user);
             }
 
-            setUser(user);
-            
-            // showToast('Login successful', ToastType.Success);
+            showToast('Login successful', ToastType.Success);
             navigate('/dashboard');
         } catch (error) {
             showToast((error as Error).message, ToastType.Error);
