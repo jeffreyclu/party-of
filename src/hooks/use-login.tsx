8304467425e9e0b875d9ae70bcb0da22027cbebe
Jeffrey Lu <hi@jeffreyclu.com>
2024-09-15 21:33:37 -0400

    show toast on login

--- src/hooks/use-login.tsx
+++ src/hooks/use-login.tsx
@@ -3,21 +3,27 @@ import { auth, provider } from '../firebase/index';
 import { signInWithPopup, setPersistence, browserLocalPersistence } from 'firebase/auth';
 import { useUser } from './use-user';
 import { createUserProfile } from '../firebase/user-functions';
+import { useToast } from './use-toast';
+import { ToastType } from '../types';
 
 export const useLogin = () => {
     const navigate = useNavigate();
     const { setUser } = useUser();
+    const { showToast } = useToast();
 
     const handleLogin = async () => {
         try {
             await setPersistence(auth, browserLocalPersistence);
             const result = await signInWithPopup(auth, provider);
             const user = result.user;
+
+            // showToast('Login successful', ToastType.Success);
+
             setUser(user);
             createUserProfile(user);
             navigate('/dashboard');
         } catch (error) {
-            console.error("Error logging in: ", error);
+            showToast((error as Error).message, ToastType.Error);
         }
     };
 
