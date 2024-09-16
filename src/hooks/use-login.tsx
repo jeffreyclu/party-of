a6e59c36839b260d7bab0ebac5dd1ba0611b3ff5
Jeffrey Lu <hi@jeffreyclu.com>
2024-09-15 22:08:07 -0400

    check profile exists before creating on login

--- src/hooks/use-login.tsx
+++ src/hooks/use-login.tsx
@@ -1,8 +1,9 @@
 import { useNavigate } from 'react-router-dom';
+
 import { auth, provider } from '../firebase/index';
 import { signInWithPopup, setPersistence, browserLocalPersistence } from 'firebase/auth';
 import { useUser } from './use-user';
-import { createUserProfile } from '../firebase/user-functions';
+import { createUserProfile, getUserProfile } from '../firebase/user-functions';
 import { useToast } from './use-toast';
 import { ToastType } from '../types';
 
@@ -17,10 +18,15 @@ export const useLogin = () => {
             const result = await signInWithPopup(auth, provider);
             const user = result.user;
 
-            // showToast('Login successful', ToastType.Success);
+            const userProfile = await getUserProfile(user.uid)
+
+            if (!userProfile) {
+                createUserProfile(user);
+            }
 
             setUser(user);
-            createUserProfile(user);
+            
+            // showToast('Login successful', ToastType.Success);
             navigate('/dashboard');
         } catch (error) {
             showToast((error as Error).message, ToastType.Error);
